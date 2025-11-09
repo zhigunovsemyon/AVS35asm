0d0669a4677dff59247fac9a88e541d921c8432e
zhigunovsemyon <zhigunovsemen@gmail.com>
2025-11-09 10:45:13 +0300

    Сделал consteval функцию подсчёта нужного места, сложение i8

--- src/main.cc
+++ src/main.cc
@@ -13,6 +13,19 @@ typedef uint16_t u16;
 typedef int32_t i32;
 typedef uint32_t u32;
 
+template <std::integral T>
+consteval uint16_t int_count_space() 
+{
+	uint16_t ret = 0;
+	auto val = std::numeric_limits<T>::max();
+
+	while (val) {
+		val /= 10;
+		ret++;
+	}
+	return ret + ((std::numeric_limits<T>::min() < 0) ? 1 : 0);
+}
+
 template <std::floating_point T> void print_array(std::span<T> A)
 {
 	for (auto n : A)
@@ -23,7 +36,7 @@ template <std::floating_point T> void print_array(std::span<T> A)
 template <std::integral T> void print_array(std::span<T> A)
 {
 	for (auto n : A)
-		std::print("{:11} ", n);
+		std::print("{:{}} ", n, int_count_space<T>() + 1);
 	std::println();
 }
 
@@ -75,6 +88,30 @@ static void sse_u16_sub()
 	print_array(std::span{c, 8});
 }
 
+static void sse_i8_sum()
+{
+	using type = i8;
+	puts("Задание 3. i8 C = A + B");
+	type a[16] = {0, 1, std::numeric_limits<type>::max(), 11, -120, 4, 99, 1, 12, -77, 11, 12, -13, 14, -88};
+	type b[16] = {1, 38, 4, 125, 0, 2, 19, std::numeric_limits<type>::min(), -99, 100, 111, -12, 26, -28, 66};
+	type c[16] = {};
+
+	printf("a: ");
+	print_array(std::span{a, 16});
+	printf("b: ");
+	print_array(std::span{b, 16});
+
+	_asm {
+		movdqu xmm0, a
+		movdqu xmm1, b
+		paddb xmm0, xmm1
+		movdqu c, xmm0
+	}
+
+	printf("c: ");
+	print_array(std::span{c, 16});
+}
+
 static void sse_f32_mult()
 {
 	puts("Задание 4. f32 C = A * B");
@@ -102,8 +139,13 @@ int main()
 {
 	sse_i32_sum();
 	std::println();
+	
 	sse_u16_sub();
 	std::println();
+
+	sse_i8_sum();
+	std::println();
+
 	sse_f32_mult();
 
 	puts("Нажмите любую кнопку для завершения");
